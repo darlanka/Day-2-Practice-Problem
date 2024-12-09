14ad989a7aaf610a63876dcb1204516b1fcd8e44
Praneeth <darlanka21_ug@ece.nits.ac.in>
2024-12-10 02:21:19 +0530

    Added : wage per month

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome! to Employee Wage Computation");
+        int workingdaypermonth = 20;
+        int wage_per_hour =  20;
+        int Full_day_hour = 8;
+        // assuming the employee is present!
+        // so 20 days == 20 * 20hours == 20 * 20 * 8 which is the cost
+
+        int ans = workingdaypermonth*wage_per_hour*Full_day_hour;
+        System.out.println("the total wage of the Employee per month is " + ans);
     }
 }
