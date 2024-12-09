fbf58c0c8284bf0be29a61422255d0c2c57d90f5
Praneeth <darlanka21_ug@ece.nits.ac.in>
2024-12-10 02:17:26 +0530

    Added: Sloved using the switch statements

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome! to Employee Wage Computation");
+
+        int parttimeornot = (int) (Math.random()*2); // generates a random value 0 or 1
+        int wagePerHour = 20;
+        int FullDayHour = 8;
+        int parttimehour = 2;
+        int parttimepay_perhour = 8;
+        int general_pay = wagePerHour*FullDayHour;
+        switch (parttimeornot){
+            case 1 -> System.out.println("the part time payment is added so total pay is " + general_pay + parttimehour*parttimepay_perhour);
+            case 0 -> System.out.println("the general pay is " + general_pay);
+            default -> System.out.println("You are no longer an employee please contact administration!");
+        }
+
     }
 }
